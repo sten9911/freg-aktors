de96b09fa483c7bfc7da573380b229b0295609c8
unknown <sten9911@gmail.com>
2024-10-17 17:25:54 +0300

    working on header and setting up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FunctionsComponent } from './components/functions/functions.component';
+import { DetailsComponent } from './components/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'functions', component: FunctionsComponent},
+    {path: 'details', component: DetailsComponent},
+    {path: '', redirectTo:'/home', pathMatch:'full'}
+];
