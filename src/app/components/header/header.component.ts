ab29e2ed9aa9ae479c03aed05525be997fac6a08
unknown <sten9911@gmail.com>
2024-10-18 16:53:37 +0300

    working on header, footer and homepage

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SearchBarComponent } from '../misc/search-bar/search-bar.component';
+import { ToggleButtonComponent } from '../misc/toggle-button/toggle-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ToggleButtonComponent, SearchBarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  darkmodeValue: boolean = false;
 }
